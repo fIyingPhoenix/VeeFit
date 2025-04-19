868f87d69abc18efd2a0e3d8e1403716cdbeb8c3
FUJITSU-LIFEBOO\Student <kamil_kara@hotmail.de>
2025-04-19 18:43:12 +0200

    Update

--- VeeFit.Desktop/src/main/java/app/veefit/veefitdesktop/mainController.java
+++ VeeFit.Desktop/src/main/java/app/veefit/veefitdesktop/mainController.java
@@ -3,7 +3,7 @@ package app.veefit.veefitdesktop;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class mainController {
     @FXML
     private Label welcomeText;
 
