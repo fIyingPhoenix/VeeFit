1ebd15adb20a94bdfbf4dd04cbe46505b4d9a835
Kamil <kamil_kara@hotmail.de>
2025-04-29 14:23:35 +0200

    feat (GUI) automatic date label

--- VeeFit.Desktop/src/main/java/app/veefit/veefitdesktop/Controllers/Client/DashboardController.java
+++ VeeFit.Desktop/src/main/java/app/veefit/veefitdesktop/Controllers/Client/DashboardController.java
@@ -6,23 +6,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Region;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.PieChart;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DashboardController {
 
     public PieChart kalorienChart;
+    @FXML
+    public Label dateLabel;
+    public Text welcome_text;
 
     public void initialize() {
+
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        dateLabel.setText("Heute: " + today.format(formatter));
 
         ObservableList<PieChart.Data> daten = FXCollections.observableArrayList(
                 new PieChart.Data("Gegessen", 1200),
